128b5f7f3c7bc5ef75b4020efea51ce18f70af08
NCStorey <ncsansom@hotmail.co.uk>
2023-07-13 14:17:05 +0100

    addition of javascript for window button

--- script.js
+++ script.js
@@ -10,30 +10,36 @@ const response_one_div = document.getElementById("response_one_div");
 let user_name = prompt("Please enter your name :) if you're not sure then leave it blank and I will call you, hmmmmmm . . . . Friend!")
 
 if (user_name === ""){
-  user_name = "friend"
+    user_name = "friend"
 }
 
 name_input_here.innerText = user_name
 
 console.log(user_name)
 
-yes_get_ready.addEventListener("click", function(){
-  
-  const yes_para = document.createElement("p");
-  yes_para.innerText = "Yay, thank you! Let's look outside the window together";
-  response_one_div.appendChild(yes_para);
-  button_div_one.style.display = "none"
-  
-  })
-
 no_get_ready.addEventListener("click", function(){
-  
-  const no_para = document.createElement("p");
-  no_para.innerText = "Awh! Ok nevermind. Maybe next time?";
-  response_one_div.appendChild(no_para);
-  button_div_one.style.display = "none"
-  })
-  
+
+    const no_para = document.createElement("p");
+    no_para.innerText = "Awh! Ok nevermind. Maybe next time?";
+    response_one_div.appendChild(no_para);
+    button_div_one.style.display = "none"
+})
+
+
+yes_get_ready.addEventListener("click", function(){
+
+    const yes_para = document.createElement("p");
+    yes_para.innerText = "Yay, thank you! Let's look outside the window together";
+    response_one_div.appendChild(yes_para);
+    button_div_one.style.display = "none";
+
+    const window_button = document.createElement("button");
+    window_button.innerText = "Look outside the window";
+    response_one_div.appendChild(window_button);
+
+})
+
+
 
 
 
