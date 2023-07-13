98a979eca2f6ddbbb9a675a8ad24db0ac5d39f30
NCStorey <ncsansom@hotmail.co.uk>
2023-07-13 14:02:39 +0100

    addition of code form code pen

---
+++ script.js
@@ -0,0 +1,40 @@
+let scenarios = ["Raining", "Sunny", "Snowing", "Bedtime"];
+
+let items = [];
+
+const name_input_here = document.getElementById("name_input_here");
+const yes_get_ready = document.getElementById("yes_get_ready");
+const no_get_ready = document.getElementById("no_get_ready");
+const response_one_div = document.getElementById("response_one_div");
+
+let user_name = prompt("Please enter your name :) if you're not sure then leave it blank and I will call you, hmmmmmm . . . . Friend!")
+
+if (user_name === ""){
+  user_name = "friend"
+}
+
+name_input_here.innerText = user_name
+
+console.log(user_name)
+
+yes_get_ready.addEventListener("click", function(){
+  
+  const yes_para = document.createElement("p");
+  yes_para.innerText = "Yay, thank you! Let's look outside the window together";
+  response_one_div.appendChild(yes_para);
+  button_div_one.style.display = "none"
+  
+  })
+
+no_get_ready.addEventListener("click", function(){
+  
+  const no_para = document.createElement("p");
+  no_para.innerText = "Awh! Ok nevermind. Maybe next time?";
+  response_one_div.appendChild(no_para);
+  button_div_one.style.display = "none"
+  })
+  
+
+
+
+
