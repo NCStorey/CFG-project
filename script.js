c20cc6897772ec9945a8e4ae4e9b428138522f6d
NCStorey <ncsansom@hotmail.co.uk>
2023-07-16 17:06:56 +0100

    tried to fix scope problems

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const name_input_here = document.getElementById("name_input_here");
-const yes_get_ready = document.getElementById("yes_get_ready");
-const no_get_ready = document.getElementById("no_get_ready");
-const response_one_div = document.getElementById("response_one_div");
+const response_div_one = document.getElementById("response_div_one");
 const start_button = document.getElementById("start_button");
 const greeting_para = document.getElementById("greeting_para");
 const window_div = document.getElementById("window_div");
@@ -47,53 +45,52 @@ start_button.addEventListener("click", function(){
     // enters the users input into the greeting paragraph
     name_input_here.innerText = user_name
 
-    // hides the start button, shows greeting paragraph and yes/no buttons on prompt closure
+    // hides the start button
     start_button.style.display = "none";
     greeting_para.style.visibility = "visible";
-    button_div_one.style.visibility = "visible";
 
-    
-})
+    // creates yes no buttons
+    const yes_get_ready = document.createElement("button");
+    yes_get_ready.innerText = "yes";
+    button_div_one.appendChild(yes_get_ready);
 
+    const no_get_ready = document.createElement("button");
+    no_get_ready.innerText = "no";
+    button_div_one.appendChild(no_get_ready);
 
-no_get_ready.addEventListener("click", function(){
+    // adds event listener to no button. if selected will end game
+    no_get_ready.addEventListener("click", function(){
 
     greeting_para.style.display = "none";
-
-    const no_para = document.createElement("p");
+    no_para = document.createElement("p");
     no_para.innerText = "Awh! Ok nevermind. Maybe next time?";
-    response_one_div.appendChild(no_para);
-    button_div_one.style.display = "none"
-
-    
+    response_div_one.appendChild(no_para);
+    button_div_one.style.display = "none";    
 })
 
-
-yes_get_ready.addEventListener("click", function(){
+    yes_get_ready.addEventListener("click", function(){
 
     greeting_para.style.display = "none";
 
-    const yes_para = document.createElement("p");
+    yes_para = document.createElement("p");
     yes_para.innerText = "Yay, thank you! Lets take a look outside. Can you click on the window please?";
-    yes_para.id = "yes_para"
-    response_one_div.appendChild(yes_para);
+    response_div_one.appendChild(yes_para);
     button_div_one.style.display = "none";
 
-    const window_img = document.createElement("img");
+    window_img = document.createElement("img");
     window_img.src = "../images/window-frame-2075509_1280.png";
     window_img.alt = "image of and empty window";
     window_img.id = "window_img"
     window_div.append(window_img);
-
+})
 
 })
 
-const window_img = document.getElementById("window_img")
 
 window_div.addEventListener("click", function(){
 
     // hiding the yes paragraph as no longer needed
-    document.getElementById("yes_para").style.display = "none"
+    
     
     // generates a random number to create a random scenario
     let scenario_num = Math.floor(Math.random() * (scenarios.length));
@@ -107,7 +104,11 @@ window_div.addEventListener("click", function(){
     condition_img.id = ("condition_img")
     window_div.append(condition_img);
 
-}, {once : true})
+    if (response_div_one.hasChildNodes()){
+        response_div_one.removeChild(response_div_one.childNodes[0])
+    }
+
+}, {once : true});
 
 
 
