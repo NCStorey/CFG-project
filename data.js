a9d9959a084990754ea93ea7812a11bd20c4db70
NCStorey <ncsansom@hotmail.co.uk>
2023-07-26 18:28:50 +0100

    bug fix 2

--- data.js
+++ data.js
@@ -18,28 +18,28 @@ const response_para_three = document.getElementById("response_para_three") ;
 let scenarios = [
     {
     "condition" : "Raining",
-    "src" : "../images/man-3581659_1280.jpg",
+    "src" : "./images/man-3581659_1280.jpg",
     "alt" : "raining",
     "reaction" : "Wow! it looks really wet and rainy out there. I can't wait to go and splash in some puddles! Can you click on what I will need?",
     "matchResponse": "Good thinking, wellington boots will keep my feet dry while I splash in the puddles - no one wants soggy socks! Thank you for your help ",
     }, 
     {
     "condition" : "Sunny",
-    "src" : "../images/background-2184_1280.jpg",
+    "src" : "./images/background-2184_1280.jpg",
     "alt" : "sunny field",
     "reaction" : "it is so warm and sunny outside, I can't wait to go and explore! Can you click on what I will need?",
     "matchResponse": "Good thinking, I need to put my suntan cream on before going out to keep my skin safe. My hat will keep the sun out of my eye's and protect my head. Thank you for your help",
     },
     {
     "condition" : "Bedtime",
-    "src" : "../images/british-columbia-2382640_1280.jpg",
+    "src" : "./images/british-columbia-2382640_1280.jpg",
     "alt" : "stars across a dark sky",
     "reaction" : "it is so dark outside, it must be bedtime. Can you click on what I will need?",
     "matchResponse": "This will keep me nice and snuggly while I sleep. I am glad you remembered my teddy bear, I love to hug him as I go to sleep, he makes me feel safe and happy. Time for me to brush my teeth - good night",
     },
     {
     "condition" : "Snowing",
-    "src" : "../images/winter-landscape-2571788_1280.jpg",
+    "src" : "./images/winter-landscape-2571788_1280.jpg",
     "alt" : "a winters day",
     "reaction" : "Brrrrr it looks cold outside! I love making snow angels and sledging down hills. Can you click on what I will need?",
     "matchResponse": "Good thinking, I will be so nice and warm while I play in the snow. I am going to go and make a snow fort. Thank you for your help ",
@@ -51,22 +51,22 @@ let head_item_list = [
     {
     "item" : "Sun Hat", 
     "condition" : "Sunny",
-    "src" : "../images/items/head/sunhats.png",
+    "src" : "./images/items/head/sunhats.png",
     },
     {
         "item" : "Rain Hat", 
         "condition" : "Raining",
-        "src" : "../images/items/head/rainhats.png",
+        "src" : "./images/items/head/rainhats.png",
     },
     {
     "item" : "Sleep Cap", 
     "condition" : "Bedtime",
-    "src" : "../images/items/head/nightcaps.png",
+    "src" : "./images/items/head/nightcaps.png",
     },
     {
     "item" : "Bobble hat",
     "condition" : "Snowing",
-    "src" : "../images/items/head/bobblehats.png",
+    "src" : "./images/items/head/bobblehats.png",
     }
 ]
 
@@ -74,22 +74,22 @@ let body_item_list = [
     {
         "item" : "Pajamas", 
         "condition" : "Bedtime",
-        "src" : "../images/items/body/pajamass.png",
+        "src" : "./images/items/body/pajamass.png",
     },
     {
         "item" : "Warm Coat",
         "condition" : "Snowing",
-        "src" : "../images/items/body/warmcoats.png",
+        "src" : "./images/items/body/warmcoats.png",
     },
     {
     "item" : "Rain Coat", 
     "condition" : "Raining",
-    "src" : "../images/items/body/raincoats.png",
+    "src" : "./images/items/body/raincoats.png",
     },
     {
     "item" : "T-shirt", 
     "condition" : "Sunny",
-    "src" : "../images/items/body/tshirts.png",
+    "src" : "./images/items/body/tshirts.png",
     },
 ]
 
@@ -97,22 +97,22 @@ let hold_item_list = [
     {
         "item" : "Sun Cream", 
         "condition" : "Sunny",
-        "src" : "../images/items/hold/suncreams.png",
+        "src" : "./images/items/hold/suncreams.png",
     },
     {
     "item" : "Umbrella", 
     "condition" : "Raining",
-    "src" : "../images/items/hold/umbrellas.png",
+    "src" : "./images/items/hold/umbrellas.png",
     },
     {
     "item" : "Teddy", 
     "condition" : "Bedtime",
-    "src" : "../images/items/hold/teddys.png",
+    "src" : "./images/items/hold/teddys.png",
     },
     {
     "item" : "Gloves",
     "condition" : "Snowing",
-    "src" : "../images/items/hold/glovess.png",
+    "src" : "./images/items/hold/glovess.png",
     }
 ]
 
@@ -120,22 +120,22 @@ let foot_item_list = [
     {
         "item" : "Boots",
         "condition" : "Snowing",
-        "src" : "../images/items/foot/snowbootss.png",
+        "src" : "./images/items/foot/snowbootss.png",
     },
     {
         "item" : "Slippers", 
         "condition" : "Bedtime",
-        "src" : "../images/items/foot/slipperss.png",
+        "src" : "./images/items/foot/slipperss.png",
     },
     {
         "item" : "Sandals", 
         "condition" : "Sunny",
-        "src" : "../images/items/foot/sandalss.png",
+        "src" : "./images/items/foot/sandalss.png",
     },
     {
     "item" : "Wellington Boots", 
     "condition" : "Raining",
-    "src" : "../images/items/foot/wellingtons.png",
+    "src" : "./images/items/foot/wellingtons.png",
     },
 
 ]
